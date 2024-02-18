c2f336f020da7ea57e3c7b130a2bfd813639d7e7
KateZ <hello.katezhang@gmail.com>
2024-02-17 20:16:13 -0500

    Rely on Toaster as toast solution

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { toast } from 'sonner';
 
 import AuthRoute from './pages/auth';
 import BookingsRoute from './pages/bookings';
@@ -7,23 +8,25 @@ import AppLayout from './shared/components/app-layout';
 
 export default function App() {
   return (
-    <Routes>
-      {/* The good practice is to name application as {app|portal}.example.com */}
-      {/* Yet, if you do not want follow that pattern, naming app as prefix is a good way too */}
-      <Route element={<Navigate to='/auth' />}
-        path='/' />
-      <Route element={<AuthRoute />}
-        path='/auth/*' />
-      <Route element={<AppLayout />}
-        path='/app'>
-        <Route element={<Navigate to='settings' />}
-          index
-          path='' />
-        <Route element={<SettingsRoute />}
-          path='settings/*' />
-        <Route element={<BookingsRoute />}
-          path='bookings/*' />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        {/* The good practice is to name application as {app|portal}.example.com */}
+        {/* Yet, if you do not want follow that pattern, naming app as prefix is a good way too */}
+        <Route element={<Navigate to='/auth' />}
+          path='/' />
+        <Route element={<AuthRoute />}
+          path='/auth/*' />
+        <Route element={<AppLayout />}
+          path='/app'>
+          <Route element={<Navigate to='settings' />}
+            index
+            path='' />
+          <Route element={<SettingsRoute />}
+            path='settings/*' />
+          <Route element={<BookingsRoute />}
+            path='bookings/*' />
+        </Route>
+      </Routes>
+    </>
   );
 }
--- src/shared/components/app-providers/app-providers.tsx
+++ src/shared/components/app-providers/app-providers.tsx
@@ -1,16 +1,24 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner';
 
 import { queryClient } from './query-client';
 
 export default function AppProviders({ basename, children }: Props) {
   return (
-    <BrowserRouter basename={basename ?? ''}>
-      <QueryClientProvider client={queryClient}>
-        <Suspense fallback={null}>{children}</Suspense>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename={basename ?? ''}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={null}>{children}</Suspense>
+        </QueryClientProvider>
+      </BrowserRouter>
+      {/* configuration https://ui.shadcn.com/docs/components/sonner */}
+      {/* No need to wrap it inside, as it doesn't rely on context as the media to communicate */}
+      <Toaster position='top-right'
+        richColors
+      />
+    </>
   );
 }
 
